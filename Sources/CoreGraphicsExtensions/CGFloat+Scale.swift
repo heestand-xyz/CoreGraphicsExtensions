a64b26e06b6f95fd0bc04cdd97d1466d6167634a
Anton Heestand <anton.heestand@gmail.com>
2025-01-20 14:15:19 +0900

    Estimated Pixels per Point

--- Sources/CoreGraphicsExtensions/CGFloat+Scale.swift
+++ Sources/CoreGraphicsExtensions/CGFloat+Scale.swift
@@ -34,3 +34,35 @@ public extension CGFloat {
     }()
 }
 
+public extension CGFloat {
+   
+    /// The estimation does not require MainActor access.
+    static var estimatedPointsPerPixel: CGFloat {
+        1.0 / estimatedPixelsPerPoint
+    }
+    
+    /// The estimation does not require MainActor access.
+    ///
+    /// iOS, visionOS: 3 pixels per point
+    ///
+    /// macOS, watchOS, tvOS: 2 pixels per point
+    ///
+    /// Note that visionOS has dynamic pixels per points,
+    /// so the estimation here is a best guess at an average.
+    /// 
+    /// The estimation is does not consider older devices.
+    static let estimatedPixelsPerPoint: CGFloat = {
+#if os(macOS)
+        2.0
+#elseif os(watchOS)
+        2.0
+#elseif os(visionOS)
+        3.0
+#elseif os(iOS)
+        3.0
+#elseif os(tvOS)
+        2.0
+#endif
+    }()
+}
+
